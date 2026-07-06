aa6e031802fa99b3679d2f1a5e52b6ecc45355a2
iauyutoph <Connect.SUi1423@gmail.com>
2026-07-06 09:53:31 +0900

    Add template with utility functions for programming
    This file includes various utility functions for character indexing, power calculations, and modular arithmetic. It sets up a template for competitive programming.

---
+++ Template/template.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+const string ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string abc = "abcdefghijklmnopqrstuvwxyz";
+const ll mod = 998244353;
+const int INF = 2e9;
+const ll INFL = 2e18;
+const int dx[5] = {-1, 0, 1, 0}, dy[5] = {0, 1, 0, -1};
+#define rep0(i, n) for (int i = 0; i < (int)n; i++)
+#define rep1(i, n) for (int i = 1; i <= (int)n; i++)
+#define rrep1(i, n) for (int i = n; i > 0; i--)
+#define rrep0(i, n) for (int i = n - 1; i >= 0; i--)
+#define el "\n"
+#define coutY cout << "Yes" << el
+#define coutN cout << "No" << el
+#define all(x) x.begin(), x.end()
+
+template<typename T> bool chmin(T &a, T b) {return ((a > b) ? (a = b, true) : (false));}
+template<typename T> bool chmax(T &a, T b) {return ((a < b) ? (a = b, true) : (false));}
+template<typename T> 
+using pq_greater = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> 
+using pq_less = priority_queue<T, vector<T>, less<T>>;
+
+//文字cのABC/abc文字列内でのitrを求める
+int char_itr(char c, string ABC_abc) {
+    int ans = 0;
+    rep0(i, 26) if (c == ABC_abc[i]) ans = i;
+    if (ABC_abc[ans] != c) ans = -1;
+    return ans;
+}
+
+//x^nを求める
+ull power(ull x, ull n) {
+    ull ans = 1;
+    while (n > 0) {
+        if (n & 1) ans *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return ans;
+}
+
+//x^nをmod998244353で求める
+ull modpower(ull x, ull n) {
+    ull ans = 1;
+    while (n > 0) {
+        if (n & 1) ans = ans * x % mod;
+        x = x * x % mod;
+        n >>= 1;
+    }
+    return ans;
+}
+
+//xのmod998244353における逆元を求める
+ll modinv(ll x) {
+    return modpower(x, mod - 2);
+}
+
+void solve() {
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+    return 0;
+}
